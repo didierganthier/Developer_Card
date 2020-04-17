509b2d64eff76915eeecfc5f2ad05e3d7b2add1c
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-04-17 19:08:08 -0400

    Challenge Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,121 @@
 import 'package:flutter/material.dart';
+import 'assets/my_app_icons.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.blue,
+                  backgroundImage: AssetImage('images/profile.jpg'),
+                ),
+                Text(
+                  'Didier Ganthier',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'SourceSansPro',
+                    letterSpacing: 2.5
+                  ),
+                ),
+                Divider(
+                  color: Colors.white,
+                  indent: 40.0,
+                  endIndent: 40.0,
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      MyCustomIcons.whatsapp,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "+509 4419-3238",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 16.0,
+                        fontFamily: 'SourceSansPro',
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "didierganthierperan@gmail.com",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 16.0,
+                        fontFamily: 'SourceSansPro',
+                        ),
+                      ),
+                    ),
+                  ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      MyCustomIcons.twitter,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "didierganthier_",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 16.0,
+                        fontFamily: 'SourceSansPro',
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      MyCustomIcons.github,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "didierganthier",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 16.0,
+                        fontFamily: 'SourceSansPro',
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
+
